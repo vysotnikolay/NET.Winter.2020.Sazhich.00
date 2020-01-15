264810a175e10592424a7fc9bf166e6d832fed9b
Nikolay <vysotnikolay@gmail.com>
2020-01-15 13:43:26 +0300

    Methods implementation added

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,12 +2,43 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// Provides static methods strings.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// Returns the vowels count contained in the input string.
+        /// </summary>
+        /// <param name="str">Input string.</param>
+        /// <returns>Vovels count.</returns>
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str), "String cannot be null.");
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("String cannot be empty.");
+            }
+
+            int vovelCounter = 0;
+            char[] vovelSymbolsArray = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            char[] strCharsArray = str.ToCharArray();
+            for (int i = 0; i < vovelSymbolsArray.Length; i++)
+            {
+                for (int j = 0; j < strCharsArray.Length; j++)
+                {
+                    if (vovelSymbolsArray[i] == strCharsArray[j])
+                    {
+                        vovelCounter++;
+                    }
+                }
+            }
+
+            return vovelCounter;
         }
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,13 +1,32 @@
-using System;
+﻿using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// Provide methods with integers.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// Returns the GCD of two integers.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GSD value.</returns>
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+
+            int tempB = a % b;
+            return GetGcd(b, tempB);
         }
     }
 }
